11a2d43ff9a8ce03706a1a31848aac34cf920c62
arslan0060 <amemon@smu.edu>
2017-11-18 08:06:54 -0600

    started pg

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,16 @@
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule

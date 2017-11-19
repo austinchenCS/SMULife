db61ecf9e7124ba32dc4eb9d9a0d41bab2971630
arslan0060 <amemon@smu.edu>
2017-11-19 17:03:26 -0600

    events is up

--- frontend/src/app/components/components-routes.ts
+++ frontend/src/app/components/components-routes.ts
@@ -1,3 +1,4 @@
+import { EventsComponent } from './events/events.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { ResidentsPageComponent } from './residents-page/residents-page.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -6,5 +7,6 @@ import { Routes } from "@angular/router";
 export const COMPONENTS_ROUTES : Routes = [
     { path: 'profile', component: ProfileComponent },
     { path: 'residents', component: ResidentsPageComponent },
-    { path: 'frontpage', component: FrontpageComponent }
+    { path: 'frontpage', component: FrontpageComponent },
+    { path: 'events', component: EventsComponent}
 ];
--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { DomainModule } from '../domain';
 import { COMPONENTS_ROUTES } from './components-routes';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
     imports: [
@@ -22,7 +23,8 @@ import { COMPONENTS_ROUTES } from './components-routes';
         FooterComponent,
         FrontpageComponent,
         ProfileComponent,
-        ResidentsPageComponent
+        ResidentsPageComponent,
+        EventsComponent
     ],
     exports: [
     ]
